45e4b540830bd0b23a8c6f27e5160318c35a225b
Sandeep maurya <snmmaurya@gmail.com>
2016-10-13 15:03:12 +0530

    user signin feature completed

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "snm@snmmaurya.com"
+    password "snmmaurya"
+  end
+end
---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+
+RSpec.feature "User can singin" do
+  let!(:user) {FactoryGirl.create(:user)}
+
+  before do
+    visit "/"
+  end  
+
+  scenario "with valid information" do
+    click_link "Sign in"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Signed in as #{user.email}")
+  end
+
+  scenario "with invallid information" do
+    click_link "Sign in"
+    
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    click_button "Log in"
+
+    expect(page).to have_content("Invalid email or password.")
+  end  
+end  
